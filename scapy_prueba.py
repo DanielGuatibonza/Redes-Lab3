0ea06529de489c344a8082cd4597ada53b3e4fa6
DanielGuatibonza <dm.guatibonza@uniandes.edu.co>
2021-09-26 20:43:01 -0500

    Cambio 23

--- scapy_prueba.py
+++ scapy_prueba.py
@@ -1,13 +1,15 @@
 import scapy.all as scapy
   
-request = scapy.ARP()
+# request = scapy.ARP()
   
-request.pdst = 'x'
-broadcast = scapy.Ether()
+# request.pdst = 'x'
+# broadcast = scapy.Ether()
   
-broadcast.dst = 'ff:ff:ff:ff:ff:ff'
+# broadcast.dst = 'ff:ff:ff:ff:ff:ff'
   
-request_broadcast = broadcast / request
-clients = scapy.srp(request_broadcast, timeout = 1)[0]
-for element in clients:
-    print(element[1].psrc + "      " + element[1].hwsrc)
+# request_broadcast = broadcast / request
+# clients = scapy.srp(request_broadcast, timeout = 1)[0]
+# for element in clients:
+#     print(element[1].psrc + "      " + element[1].hwsrc)
+
+print(scapy.sniff(count=1, iface="ens33", prn=lambda x: x.show()))
