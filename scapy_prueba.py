46bcefee69e0a0a9f369e67f2f3f2a94615d53c6
DanielGuatibonza <dm.guatibonza@uniandes.edu.co>
2021-09-26 22:42:52 -0500

    Cambio 51

--- scapy_prueba.py
+++ scapy_prueba.py
@@ -2,17 +2,20 @@ import scapy.all as scapy
 from scapy.all import TCP
 from scapy.layers.l2 import Ether
 
+tipos = set()
 bytes_ = []
 cont = 0
 def procesar_paquete(paquete):
     global bytes_, cont
-    length = len(paquete[TCP].__bytes__())
+    length = len(paquete[Ether].__bytes__())
     if length == 1514:
         cont += 1
     bytes_.append(length)
+    tipos.add(type(paquete))
     print("Bytes", sum(bytes_))
     print("Cantidad 1514", cont)
     print("Cantidad", len(bytes_))
+    print("Tipos", tipos)
     # print(type(x[Ether]))
     # print(dir(x[Ether]))
     # print(x.mysummary())
