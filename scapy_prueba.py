25e17e630866a92e3654f854fd28d09a644bfd63
DanielGuatibonza <dm.guatibonza@uniandes.edu.co>
2021-09-26 21:07:39 -0500

    Cambio 27

--- scapy_prueba.py
+++ scapy_prueba.py
@@ -3,5 +3,7 @@ from scapy.layers.l2 import Ether
 
 def procesar_paquete(x):
     print(type(x))
+    print(x.mysummary())
+    print(x.payload_guess)
 
 scapy.sniff(count=100, iface="ens33", prn=procesar_paquete)
