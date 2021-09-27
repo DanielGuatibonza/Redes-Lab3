bfefc6fdbaf8adc1c9e6e88071994c04b7f98e5b
DanielGuatibonza <dm.guatibonza@uniandes.edu.co>
2021-09-26 21:20:40 -0500

    Cambio 29

--- scapy_prueba.py
+++ scapy_prueba.py
@@ -2,8 +2,9 @@ import scapy.all as scapy
 from scapy.layers.l2 import Ether
 
 def procesar_paquete(x):
+    x.show()
     print(type(x))
     print(x.mysummary())
     print(x.payload_guess)
 
-scapy.sniff(count=100, iface="ens33", filter="tcp", prn=procesar_paquete)
+scapy.sniff(iface="ens33", filter="tcp", prn=procesar_paquete)
