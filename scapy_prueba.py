560c7163efbac2186567e38ed797189d3730ff22
DanielGuatibonza <dm.guatibonza@uniandes.edu.co>
2021-09-26 21:03:15 -0500

    Cambio 26

--- scapy_prueba.py
+++ scapy_prueba.py
@@ -1,18 +1,7 @@
 import scapy.all as scapy
-  
-# request = scapy.ARP()
-  
-# request.pdst = 'x'
-# broadcast = scapy.Ether()
-  
-# broadcast.dst = 'ff:ff:ff:ff:ff:ff'
-  
-# request_broadcast = broadcast / request
-# clients = scapy.srp(request_broadcast, timeout = 1)[0]
-# for element in clients:
-#     print(element[1].psrc + "      " + element[1].hwsrc)
+from scapy.layers.l2 import Ether
 
 def procesar_paquete(x):
     print(type(x))
 
-scapy.sniff(count=10, iface="ens33", prn=procesar_paquete)
+scapy.sniff(count=100, iface="ens33", prn=procesar_paquete)
