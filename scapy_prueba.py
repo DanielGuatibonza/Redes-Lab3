c4d12459a8a1353cc2898e8b70976f111134db7a
DanielGuatibonza <dm.guatibonza@uniandes.edu.co>
2021-09-26 21:43:53 -0500

    Cambio 36

--- scapy_prueba.py
+++ scapy_prueba.py
@@ -2,9 +2,12 @@ import scapy.all as scapy
 from scapy.all import TCP
 from scapy.layers.l2 import Ether
 
+suma = 0
 def procesar_paquete(x):
+    global suma
+    suma += len(x[Ether].__bytes__())
     print(len(x[Ether].__bytes__()))
-    print(x[Ether].time)
+    print(suma)
     # print(type(x[Ether]))
     # print(dir(x[Ether]))
     # print(x.mysummary())
