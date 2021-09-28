de22a4d93126ec5ef63e0ab3a10624c4e31dee15
DanielGuatibonza <dm.guatibonza@uniandes.edu.co>
2021-09-28 17:00:05 -0500

    Cambio 71

--- tshark.py
+++ tshark.py
@@ -1,7 +1,7 @@
 import time
 import subprocess
 
-proceso = subprocess.Popen(['tshark', '-d', 'tcp.port==8081'])
+proceso = subprocess.Popen(['tshark', '-d', 'tcp.port==8081', '-w', 'captura.pcap', '-F', 'pcap'])
 
 time.sleep(10)
 
