4a23c8fcf9a90990a37f8021d81f14761b5c5142
DanielGuatibonza <dm.guatibonza@uniandes.edu.co>
2021-09-28 10:21:09 -0500

    Cambio 58

--- captura_pyshark.py
+++ captura_pyshark.py
@@ -1,7 +1,9 @@
 import pyshark
 
-capture = pyshark.LiveCapture(interface='ens33')
+capture = pyshark.LiveCapture(interface='ens33', output_file='Capturas/captura.pcap')
 capture.sniff(timeout=50)
 
 for packet in capture.sniff_continuously(packet_count=5):
     print('Just arrived:', packet)
+
+capture.load_packets()
