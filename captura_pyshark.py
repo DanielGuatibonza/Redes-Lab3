1c73925809d30309d4aa5aca137f541d06e35154
DanielGuatibonza <dm.guatibonza@uniandes.edu.co>
2021-09-28 11:10:06 -0500

    Cambio 67

--- captura_pyshark.py
+++ captura_pyshark.py
@@ -5,6 +5,6 @@ capture.sniff(timeout=30)
 print(len(capture))
 print(dir(capture[0]))
 for packet in capture:
-     print(packet.captured_length())
+     print(packet.captured_length)
 #     contador += 1
 #     print(contador)
