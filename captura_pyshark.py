69b7ac1f809f14d69994967d6ea1e8915c1cbc71
DanielGuatibonza <dm.guatibonza@uniandes.edu.co>
2021-09-28 11:22:42 -0500

    Cambio 68

--- captura_pyshark.py
+++ captura_pyshark.py
@@ -1,6 +1,6 @@
 import pyshark
 
-capture = pyshark.LiveCapture(interface='ens33')
+capture = pyshark.LiveCapture(interface='ens33', bpf_filter='tcp port 8081')
 capture.sniff(timeout=30)
 print(len(capture))
 print(dir(capture[0]))
