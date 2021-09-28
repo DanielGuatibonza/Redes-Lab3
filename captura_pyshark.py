6003c2841a98c6baa2d34fbb149890734140b9fa
DanielGuatibonza <dm.guatibonza@uniandes.edu.co>
2021-09-28 10:40:53 -0500

    Cambio 64

--- captura_pyshark.py
+++ captura_pyshark.py
@@ -1,10 +1,9 @@
 import pyshark
 
 capture = pyshark.LiveCapture(interface='ens33')
-capture.sniff(timeout=50)
-
-contador = 0
-for packet in capture:
-    #print('Just arrived:', packet)
-    contador += 1
-    print(contador)
+capture.sniff(timeout=30)
+print(len(capture))
+# for packet in capture:
+#     #print('Just arrived:', packet)
+#     contador += 1
+#     print(contador)
